7df7c32f52ff4a52638c5162a46989b5f84ae4f4
Brenden Olding <oldingb21@gmail.com>
2023-04-20 00:44:42 -0400

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solution here
+const findMatching = (array, name) => {
+    const matches = array.filter((driver) => {
+       return driver.toLowerCase() === name.toLowerCase()
+       })
+    return matches  
+  }
+
+const fuzzyMatch = (array, letters) => {
+    const matches = array.filter((driver) => {
+      return driver.toLowerCase().substring(0,2) === letters.toLowerCase() || driver.toLowerCase().substring(0,1) === letters.toLowerCase()})
+    return matches;
+}
+
+const matchName = (array, person) => {
+    const matches = array.filter((driver) => {
+        return driver.name.toLowerCase() === person.toLowerCase();
+    })
+    return matches;
+}
